bbb0ed034c6fe929da42f09b3a46a3c9b3e84d6c
nickollay <nickollaysokolov@gmail.com>
2019-12-22 22:35:36 +0300

    Add 'creating and deleting post' feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 	
 	def index
-		@posts = Post.all.order('created_ DESC')
+		@posts = Post.all.order('created_at DESC')
 	end
 
 	def new
@@ -22,10 +22,29 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
+	end
+
 	private
 
 	def post_params
 		params.require(:post).permit(:title, :content)
 	end
-
 end
