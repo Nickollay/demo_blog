f0dfad4c89783a114a49649f0c203d58a261fce9
nickollay <nickollaysokolov@gmail.com>
2019-12-23 13:28:02 +0300

    Final feature 'Creating and deleting  comments for posts'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
 	
+	#call method 'find_post' for only other methonds (show, update, edit and destroy)
+	before_action :find_post, only: [:show, :update, :edit, :destroy]
+
 	def index
 		@posts = Post.all.order('created_at DESC')
 	end
@@ -19,11 +22,11 @@ class PostsController < ApplicationController
 	end
 
 	def show
-		@post = Post.find(params[:id])
+		#@post = Post.find(params[:id])
 	end
 
 	def update
-		@post = Post.find(params[:id])
+		#@post = Post.find(params[:id])
 
 		if @post.update(post_params)
 			redirect_to @post
@@ -33,11 +36,11 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		@post = Post.find(params[:id])
+		#@post = Post.find(params[:id])
 	end
 
 	def destroy
-		@post = Post.find(params[:id])
+		#@post = Post.find(params[:id])
 		@post.destroy
 		redirect_to posts_path
 	end
@@ -46,5 +49,9 @@ class PostsController < ApplicationController
 
 	def post_params
 		params.require(:post).permit(:title, :content)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
 	end
 end
